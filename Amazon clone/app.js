c6f566769aa10fea259b84ca7fb9c9379cca8b5a
Mayhem2 <barathvikram25@gmail.com>
2024-06-16 13:20:55 +0530

    onlineshop9,10

---
+++ Amazon clone/app.js
@@ -0,0 +1,32 @@
+document.querySelector(".r-btn").addEventListener("click",function (event) {
+    document.querySelector(".product-slide").scrollLeft +=1100;
+    event.preventDefault();       
+});
+document.querySelector(".l-btn").addEventListener("click",function (event) {
+    document.querySelector(".product-slide").scrollLeft -=1100;
+    event.preventDefault();       
+});
+document.querySelector(".btn-1a").addEventListener("click",function (event) {
+    document.querySelector(".product-slide-1").scrollLeft +=1100;
+    event.preventDefault();       
+});
+document.querySelector(".btn-1b").addEventListener("click",function (event) {
+    document.querySelector(".product-slide-1").scrollLeft -=1100;
+    event.preventDefault();       
+});
+document.querySelector(".btn-2a").addEventListener("click",function (event) {
+    document.querySelector(".product-slide-2").scrollLeft +=1100;
+    event.preventDefault();       
+});
+document.querySelector(".btn-2b").addEventListener("click",function (event) {
+    document.querySelector(".product-slide-2").scrollLeft -=1100;
+    event.preventDefault();       
+});
+document.querySelector(".btn-3a").addEventListener("click",function (event) {
+    document.querySelector(".product-slide-3").scrollLeft +=1100;
+    event.preventDefault();       
+});
+document.querySelector(".btn-3b").addEventListener("click",function (event) {
+    document.querySelector(".product-slide-3").scrollLeft -=1100;
+    event.preventDefault();       
+});
