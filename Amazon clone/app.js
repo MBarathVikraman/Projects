03fbd8d89b68f2bdfe26e7082b1d433d0f2221e7
Mayhem2 <barathvikram25@gmail.com>
2024-06-18 12:49:29 +0530

    onlineshop 13,14

--- Amazon clone/app.js
+++ Amazon clone/app.js
@@ -30,3 +30,37 @@ document.querySelector(".btn-3b").addEventListener("click",function (event) {
     document.querySelector(".product-slide-3").scrollLeft -=1100;
     event.preventDefault();       
 });
+document.querySelector(".backtop").addEventListener("click",()=>{
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+})
+const sidebar=document.querySelector(".sidebar");
+const cross  =document.querySelector(".fa-xmark");
+const black  =document.querySelector(".black");
+const sidebtn=document.querySelector(".second-1");
+
+sidebtn.addEventListener("click",()=>{
+    sidebar.classList.add("active");
+    cross.classList.add("active");
+    black.classList.add("active");
+    document.body.classList.add("stop-scroll");
+})
+cross.addEventListener("click",()=>{
+    sidebar.classList.remove("active");
+    cross.classList.remove("active");
+    black.classList.remove("active");
+    document.body.classList.remove("stop-scroll");
+})
+
+const sign=document.querySelector(".ac");
+const tri=document.querySelector(".triangle");
+const signin=document.querySelector(".hdn-sign");
+
+sign.addEventListener("click",()=>{
+    tri.classList.toggle("active");
+    signin.classList.toggle("active");
+    black.classList.toggle("active-1");
+    document.body.classList.toggle("stop-scroll");
+})
